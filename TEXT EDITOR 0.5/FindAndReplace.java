4ac98f844ae853716b515b3aca4c780d192bf1d9
Geethesh T G <42201809+geetheshtg@users.noreply.github.com>
2018-10-13 17:15:31 +0530

    Update FindAndReplace.java

--- TEXT EDITOR 0.5/FindAndReplace.java
+++ TEXT EDITOR 0.5/FindAndReplace.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Chumma {
+public class FindAndReplace {
 	
 	public static void main(String[] args) throws IOException{
 		Scanner sc = new Scanner(System.in);
